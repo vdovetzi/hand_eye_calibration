002a99eb69fda59ccf0a9210abc59fd150d4260f
Ilya Vdovets <ilya.vdovets@gmail.com>
2026-01-08 00:18:59 +0300

    use *operator for std::optional

--- hand_eye_calibration/src/collect_dataset.cpp
+++ hand_eye_calibration/src/collect_dataset.cpp
@@ -111,7 +111,7 @@ int main(int argc, const char **argv) {
     RCLCPP_WARN(*logger, "Error: unable to set desired logging level");
   }
   res = rcutils_logging_set_logger_level(HELPER_LOGGERNAME,
-                                         static_cast<int>(level.value()));
+                                         static_cast<int>(*level));
   if (RCUTILS_RET_OK != res) {
     RCLCPP_WARN(*logger, "Error: unable to set desired logging level");
   }
